aac2847d285a959dee1ef5184d092f23b714453b
hzariman <hzariman@gmail.com>
2022-03-26 22:43:52 -0700

    Update Settings.py
    Add assigning primary keys to models

--- klasseats/settings.py
+++ klasseats/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Assign primary keys automatically (FOR NOW)
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
